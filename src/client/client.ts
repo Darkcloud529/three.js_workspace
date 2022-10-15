cd9eb78285de9f5922d4030cf933b9c7e64bd7bd
Sangmin <bbsm8671@icloud.com>
2022-10-15 23:56:57 +0900

    three_tutorial_DRACOLoader

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,14 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
-
-// const light = new THREE.SpotLight();
-// light.position.set(5, 5, 5)
-// scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -21,16 +18,19 @@ camera.position.z = 2
 const renderer = new THREE.WebGLRenderer()
 renderer.physicallyCorrectLights = true
 renderer.shadowMap.enabled = true
-// renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/js/libs/draco/')
+
 const loader = new GLTFLoader()
+loader.setDRACOLoader(dracoLoader)
 loader.load(
-    'models/monkey.glb',
+    'models/monkey_compressed.glb',
     function (gltf) {
         gltf.scene.traverse(function (child) {
             if ((child as THREE.Mesh).isMesh) {
